400057f1dc76fbc8daf8becebab0a4023c2df334
Manice18 <manishmanice2003@gmail.com>
2024-08-31 07:40:10 +0530

    add: create bounty button in listings

--- src/app/(main)/your-listings/page.tsx
+++ src/app/(main)/your-listings/page.tsx
@@ -1,3 +1,6 @@
+import Link from "next/link";
+
+import { Button } from "@/components/ui/button";
 import YourListings from "@/components/YourListings/YourListings";
 import { constructMetaData } from "@/lib/metadata";
 
@@ -10,9 +13,14 @@ const Listings = () => {
   return (
     <section>
       <div className="mx-auto space-y-6">
-        <h1 className="text-3xl font-bold text-black dark:text-white">
-          Your Listings
-        </h1>
+        <div className="flex items-center justify-between">
+          <h1 className="text-3xl font-bold text-black dark:text-white">
+            Your Listings
+          </h1>
+          <Link href="/bounties/create-bounty">
+            <Button>Create Bounty</Button>
+          </Link>
+        </div>
         <YourListings />
       </div>
     </section>
